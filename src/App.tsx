1a4d85b6fa0abbc2796e661743e4f5307d8f7247
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-09-20 17:11:25 +0200

    header fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,51 @@
-import { useState } from 'react'
-import { Navigate, Route, Routes } from 'react-router-dom'
-import './App.css'
-import { LogIn } from './pages/LoginForm'
-import { PageNotFound } from './pages/PageNotFound'
-import { SignUp } from './pages/SIgnUpForm'
-import { Transactions } from './pages/Transactions'
+import { useState } from "react";
+import { Link, Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { LogIn } from "./pages/LoginForm";
+import { PageNotFound } from "./pages/PageNotFound";
+import { SignUp } from "./pages/SIgnUpForm";
+import { Transactions } from "./pages/Transactions";
 
-type User={
-  id: number
-  email:string
-  password: string
-}
+type User = {
+  id: number;
+  email: string;
+  password: string;
+};
 
 function App() {
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
 
   return (
     <div className="App">
       <header>
+        <Link to="/home">
         <h1>Hoxt-SBC</h1>
+        </Link>
+
+        {currentUser ? (
+          <h4>LogOut</h4>
+        ) : (
+          <div className="user-actions">
+            <Link to="/logIn">
+              <h4>LogIn</h4>
+            </Link>
+            <Link to="/signUp">
+              <h4>SignUp</h4>
+            </Link>
+          </div>
+        )}
       </header>
       <main>
         <Routes>
-        <Route index element={<Navigate to='/home' />} />
-        <Route path="/home" element={<Transactions/>}></Route>
-        <Route path="/logIn" element={<LogIn/>}></Route>
-        <Route path="/signUp" element={<SignUp/>}></Route>
-          <Route path='*' element={<PageNotFound />} />
-
+          <Route index element={<Navigate to="/home" />} />
+          <Route path="/home" element={<Transactions />}></Route>
+          <Route path="/logIn" element={<LogIn />}></Route>
+          <Route path="/signUp" element={<SignUp />}></Route>
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
