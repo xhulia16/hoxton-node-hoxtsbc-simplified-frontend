6db9c283bcce542e512170fbc5754a1ccb45d823
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-09-23 12:47:29 +0200

    commit

--- src/pages/Transactions.tsx
+++ src/pages/Transactions.tsx
@@ -1,32 +1,35 @@
-import { useEffect } from "react"
-import { User } from "../App"
+import { useEffect } from "react";
+import { User } from "../App";
 
-type Props={
-    user: User 
-}
+type Props = {
+  user: User;
+};
 
-export function Transactions({user}: Props){
-    if(user===null){
-        return(
-            <h2>Loading...</h2>
-        )
-    }
-    return(
-        <div>
-    <h1>Hello {user.email}, here your transactions for this month</h1> 
-    <ul className="transactions-list">
-        {user.transactions.map(item=>(
-            <ul className="transactions">
-                <li>{item.date}</li>
-                <li>{item.description}</li>
-                <li>{item.withdrawals}£</li>
-                <li>{item.deposits}£</li>
-                <li>{item.balance}£</li>
-            </ul>
+export function Transactions({ user }: Props) {
+  if (user === null) {
+    return <h2>Loading...</h2>;
+  }
+  return (
+    <div className="transaction-page">
+      <h1>Hello {user.email}, here your transactions for this month</h1>
+      <ul className="transactions-list">
+        <ul className="table">
+          <li>Date</li>
+          <li>Description</li>
+          <li>Withdrawals</li>
+          <li>Deposits</li>
+          <li>Balance</li>
+        </ul>
+        {user.transactions.map((item) => (
+          <ul className="transactions">
+            <li>{item.date}</li>
+            <li>{item.description}</li>
+            <li>{item.withdrawals}£</li>
+            <li>{item.deposits}£</li>
+            <li>{item.balance}£</li>
+          </ul>
         ))}
-
-    </ul>
-        </div>
-   
-    )
+      </ul>
+    </div>
+  );
 }
